747b18be853e67e7ebc1ca9a35c0950213033303
Susumu Bando <detteiu0321@gmail.com>
2018-04-28 12:38:02 +0900

    feat: add link to pr title

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'octokit'
 require 'yaml'
 require 'erb'
+require 'pp'
 
 info = YAML.load_file('./info.yml')
 
@@ -11,14 +12,15 @@ last_tuesday = today - (today.wday - 2) - 7
 
 repos = info['repositories'].map do |repo|
   prs = client.pull_requests(repo, state: 'closed')
-          .select{|pr| pr.user.login == user && pr.closed_at > last_tuesday.to_time}
-          .map(&:title)
+          .select{ |pr| pr.user.login == user && pr.closed_at > last_tuesday.to_time }
 
-  { name: repo, changes: prs }
+  changes = prs.map{ |pr| { title: pr.title, url: pr.html_url} }
+
+  { name: repo, changes: changes }
 end
 
 template = File.read('./template.md.erb')
 
-File.open("output.md","w") do |text|
-  text.puts ERB.new(template, nil, '%').result(binding)
- end
+File.open("output.md", "w") do |text|
+  text.puts ERB.new(template, nil, '-').result(binding)
+end
